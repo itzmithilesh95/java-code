25bcfa01ea115dc00735f4c9b28442ffe0517e8a
Mithilesh Kumar shah <mithileskumar9589@gmail.com>
2025-12-25 16:37:43 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,16 @@
+ import java.util.Scanner;
+// import java.scanner ;
+
+public class sum {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("inpute a");
+        int a =sc.nextInt();
+         System.out.println("inpute b");
+        int b =sc.nextInt();
+        int sum =a+b;
+        System.out.println(sum);
+        sc.close();
+    }
+    
+}
